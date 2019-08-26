1b6b543be9d997d6ad3551464f64c8f721f6481e
mariana <marianaglp15@gmail.com>
2019-08-26 16:33:29 -0500

    refactored

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -11,12 +11,10 @@ class Chronometer {
       this.currentTime = Math.floor(this.currentMilis / 100);
       this.setTime();
     }, 10);
-    btnLeft.innerText = "STOP";
-    btnRight.innerText = "SPLIT";
   }
   getMinutes = () => Math.floor(this.currentTime / 60);
 
-  getSeconds = () => this.currentTime;
+  getSeconds = () => this.currentTime % 60;
 
   twoDigitsNumber = number => `${number}`.padStart(2, "0");
 
@@ -24,50 +22,17 @@ class Chronometer {
     let minutes = this.twoDigitsNumber(this.getMinutes());
     let seconds = this.twoDigitsNumber(this.getSeconds() % 60);
     let milis = this.twoDigitsNumber(this.setMilliseconds());
-    milUni.innerText = milis[1];
-    milDec.innerText = milis[0];
-    secUni.innerText = seconds[1];
-    secDec.innerText = seconds[0];
-    minUni.innerText = minutes[1];
-    minDec.innerText = minutes[0];
-    return `${minutes}:${seconds}:${milis}`;
+    return [minutes, seconds, milis];
   }
   setMilliseconds() {
     return this.currentMilis % 100;
   }
   stopClick() {
     clearInterval(this.intervalId);
-    btnLeft.innerText = "START";
-    btnRight.innerText = "RESET";
   }
   resetClick() {
     this.currentTime = 0;
     this.currentMilis = 0;
     this.setTime();
-    splits.innerHTML = "";
-  }
-
-  splitClick() {
-    let split = document.createElement("li");
-    let splitText = document.createTextNode(chronometer.setTime());
-    split.appendChild(splitText);
-    splits.appendChild(split);
   }
 }
-
-const chronometer = new Chronometer();
-
-btnLeft.onclick = () => {
-  btnLeft.classList.toggle("start");
-  btnLeft.classList.toggle("stop");
-  btnRight.classList.toggle("reset");
-  btnRight.classList.toggle("split");
-  btnLeft.innerText === "START"
-    ? chronometer.startClick()
-    : chronometer.stopClick();
-};
-
-btnRight.onclick = () =>
-  btnRight.innerText === "RESET"
-    ? chronometer.resetClick()
-    : chronometer.splitClick();
--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -7,3 +7,45 @@ let secUni = document.getElementById("secUni");
 let milDec = document.getElementById("milDec");
 let milUni = document.getElementById("milUni");
 let splits = document.getElementById("splits");
+
+let chronometer = new Chronometer();
+
+function updateDOM() {
+  let [minutes, seconds, milis] = chronometer.setTime();
+  milUni.innerText = milis[1];
+  milDec.innerText = milis[0];
+  secUni.innerText = seconds[1];
+  secDec.innerText = seconds[0];
+  minUni.innerText = minutes[1];
+  minDec.innerText = minutes[0];
+}
+
+window.onload = () => setInterval(updateDOM, 10);
+
+btnLeft.onclick = () => {
+  btnLeft.classList.toggle("start");
+  btnLeft.classList.toggle("stop");
+  btnRight.classList.toggle("reset");
+  btnRight.classList.toggle("split");
+  if (btnLeft.innerText === "START") {
+    chronometer.startClick();
+    btnLeft.innerText = "STOP";
+    btnRight.innerText = "SPLIT";
+  } else {
+    chronometer.stopClick();
+    btnLeft.innerText = "START";
+    btnRight.innerText = "RESET";
+  }
+};
+
+btnRight.onclick = () => {
+  if (btnRight.innerText === "RESET") {
+    chronometer.resetClick();
+    splits.innerHTML = "";
+  } else {
+    let split = document.createElement("li");
+    let splitText = document.createTextNode(chronometer.setTime().join`:`);
+    split.appendChild(splitText);
+    splits.appendChild(split);
+  }
+};
